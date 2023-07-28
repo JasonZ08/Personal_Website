3b2fc5f8ab00fd81c6b6149d760f0ac316b727f7
Jason Zhong <jasonzhong0408@gmail.com>
2023-07-28 16:14:49 -0400

    Style changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,22 @@
-import './App.css';
 import {
-  BrowserRouter,
-  Route,
-  Routes,
+    BrowserRouter,
+    Route,
+    Routes,
 } from "react-router-dom";
 import Home from "./Components/Home";
-import Work from "./Components/Work";
-import Navbar from "./Components/Navbar";
-import Contact from "./Components/Contact";
-import Projects from "./Components/Projects";
-import Clubs from "./Components/Clubs";
+
 import React from 'react';
 
-
-function App (){
-  return (
-    <>
-      <BrowserRouter>
-      <Navbar/>
-        <Routes >
-          <Route path="/" element={<Home />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/clubs" element={<Clubs />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-    </BrowserRouter>
-    </>
-  );
+const App = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App;
