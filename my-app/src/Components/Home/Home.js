8425faca4dc4155ff9beeb50cc78e5f39b879a95
Jason Zhong <jasonzhong0408@gmail.com>
2023-09-16 22:22:46 -0400

    clean up

--- my-app/src/Components/Home/Home.js
+++ my-app/src/Components/Home/Home.js
@@ -1,19 +1,31 @@
-import mainPic from '../mainPic.jpeg'
 import MyNavBar from '../Navigation_Bar/Navbar';
+import Stack from 'react-bootstrap/Stack';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './home.css'
 
 function Home(){
     return (
         <div className='Home'>
-            <MyNavBar/> 
-            <h1>Hey! I'm</h1>   
-            <h1>Jason Zhong</h1>
-            <h1>A current 
-            undergraduate aiming to get some <br/>
-            real-world experience in full-stack 
-            development</h1>
-            <h1>Welcome to my website!</h1>
+            <Stack>
+                <MyNavBar/> 
+                <div className='Introduction'>
+                    <div className = 'Positioning'>
+                        <div className= 'group1'>
+                            <h1> Hey, I'm Jason Zhong! </h1>
+                            <p>
+                                <div>
+                                I'm a current undergraduate at the University of Maryland
+                                and an aspiring software engineer. Coding has always been
+                                fun for me, and I hope that my work can benefit people across the globe.
+                                </div>
+                                <div className='Separate'>Welcome to my website!</div> 
+                            </p>
+                        </div>
+                        <img width={400} height={400} src='/intro_pic.png'/>
+                    </div>
+                </div>
+                <div>Hi</div>
+            </Stack>
         </div>
     );
 }
