9e23775e78f0daeac6bdef1c70b98e22271733a3
Jason Zhong <jasonzhong0408@gmail.com>
2023-09-26 23:37:35 -0400

    edited contact

--- my-app/src/Components/Contact/Contact.js
+++ my-app/src/Components/Contact/Contact.js
@@ -8,9 +8,9 @@ function Contact() {
                 Contacts
             </h1>
             <div className="ButtonContainer py-3">
-                <img src="phone.png" width={25} height={25}/><span className="ps-2 pe-4">571-268-7291</span>
-                <img src="mail.png" width={25} height={25}/><span className="ps-2 pe-4">jasonzhong0408@gmail.com</span>
-                <img src="linkedin.png" width={25} height={25}/><span className="ps-2 pe-4"> <a href="https://www.linkedin.com/in/jason-zhong-56a4a1228/" target="_blank">LinkedIn</a></span>
+                <img src="phone.png" alt="phone" width={25} height={25}/><span className="ps-2 pe-4">571-268-7291</span>
+                <img src="mail.png" alt="mail" width={25} height={25}/><span className="ps-2 pe-4">jasonzhong0408@gmail.com</span>
+                <img src="linkedin.png" alt="linkedin" width={25} height={25}/><span className="ps-2 pe-4"> <a href="https://www.linkedin.com/in/jason-zhong-56a4a1228/" rel="noreferrer" target="_blank">LinkedIn</a></span>
             </div>
         </div>  
     )
